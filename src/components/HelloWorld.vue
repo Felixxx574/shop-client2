89f3d501500001921da2953ee1fe40ba14112c69
cara <1147824@qq.com>
2021-08-20 11:43:34 +0800

    8.20早 vue-router

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="HelloWorld">
+    <h2>HelloWorld</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  components: {},
+  data() {
+    return {
+    }
+  }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/goods.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="goods">
+    <span>商品id为:{{id}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "goods",
+  props: ["id"],
+  components: {},
+  data() {
+    return {
+
+    }
+  }
+  }
+</script>
+
+<style scoped>
+
+</style>
