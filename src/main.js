e06718bfbe9218f5be5a79366ddd89fa0f764080
cara <1147824@qq.com>
2021-08-20 09:27:43 +0800

    8.20早 vue-router

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import animate from 'animate.css'
 import './plugins/element.js'
+
 //index.js是默认不写的
 import store from './store'
+
+import router from './router'
 
 Vue.use(animate)
 
@@ -12,5 +15,6 @@ Vue.config.devtools = true
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
